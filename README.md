5cff5d6dc2dc71d969cd0a94742dae66178af8b4
lis05 <lishuang_read@126.com>
2021-11-22 20:11:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# enum
+
+## 1 枚举类生成
+payMethod.txt
+支付宝 1
+微信 1
+## 2 生成配置文件
+payMethod.xml
+
+
+
---
+++ src/main/java/com/example/loan/MyApp.java
@@ -0,0 +1,9 @@
+package com.example.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MyApp {
+
+
+}
